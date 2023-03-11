46ccfd556d0039c112d34d5b9b61bc87b4d983a3
Emer1604 <emerson_ferreira789@outlook.com>
2023-03-10 21:59:59 -0300

    Criação das classes Cliente e Endereco. Instanciamento e utilização da classe Endereco dentro da classe Cliente

--- ConsoleApp_Noite/Cliente.cs
+++ ConsoleApp_Noite/Cliente.cs
@@ -12,20 +12,23 @@ namespace ConsoleApp_Noite
         private int Id;
         private string Nome;
         private string Telefone;
+        private Endereco Endereco;
 
-        public Cliente(int id, string nome, string telefone) {
+        public Cliente(int id, string nome, string telefone, Endereco endereco) {
             Id = id;
             Nome = nome;
             Telefone = telefone;
+            Endereco = endereco;
 
         }
-
+        public Endereco getEndereco() { return Endereco; }
+        public void setEndereco(Endereco endereco) { Endereco = endereco; }
         public int getId() { return Id; }
         public void setId(int id) { Id = id;}
         public string getNome() { return Nome; }
         public void setNome (string nome) { Nome = nome; }
         public string getTelefone() { return Telefone; }
         public void setTelefone(string telefone) { Telefone = telefone; }
-
+        
     }
 }
--- ConsoleApp_Noite/Program.cs
+++ ConsoleApp_Noite/Program.cs
@@ -11,3 +11,34 @@ Console.WriteLine(p1.getDescricao()+" "+p1.getId());
 p1.setValor(11.85M);
 p1.setDescricao("Cerveja Skol 1 Litrão");
 Console.WriteLine(p1.getDescricao() + " " + p1.getId());
+
+Endereco endereco1 = new Endereco(
+    "Rua sobe e desce",
+    "894",
+    "Monte Alto",
+    "14840000",
+    "Guariba",
+    "SP"
+    );
+
+Cliente c1 = new Cliente(1, "Alan", "998756414", endereco1);
+Cliente c2 = new Cliente(2, "Bruno", "998776916", endereco1);
+Cliente c3 = new Cliente(3, "Caio", "998843461", endereco1);
+Cliente c4 = new Cliente(4, "Dioginis", "998759172", endereco1);
+Cliente c5 = new Cliente(5, "Elton", "999341414", endereco1);
+
+Console.WriteLine(c2.getEndereco().getRua() + " "+c2.getEndereco().getNumero());
+c2.getEndereco().setNumero("135");
+Console.WriteLine(c2.getEndereco().getNumero());
+
+Endereco endereco2 = new Endereco(
+    "Rua desce e sobe",
+    "498",
+    "Monte Alegre",
+    "14841000",
+    "Guariba",
+    "SP"
+    );
+Console.WriteLine(c2.getEndereco().getRua() + " " + c2.getEndereco().getNumero());
+c2.setEndereco(endereco2);
+Console.WriteLine(c2.getEndereco().getRua() + " " + c2.getEndereco().getNumero());
